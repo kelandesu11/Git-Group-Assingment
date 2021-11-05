2bfebf0182df761d41bbfda28d3d1964b54e99e1
sanjukhanal <khanal.sanju@gmail.com>
2021-11-05 00:55:33 -0400

    SK: Java style array declaration

--- src/Controller.java
+++ src/Controller.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Controller {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
 		ActionsHandler control = new ActionsHandler();
