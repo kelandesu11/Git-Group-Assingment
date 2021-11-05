f697d7d7d739d91f5745aa8d8a473d0039484ea2
Jeremy <jstiff@ggc.edu>
2021-11-04 22:04:29 -0400

    Changes to data Structure. Added true controller class.

--- src/Controller.java
+++ src/Controller.java
@@ -5,76 +5,11 @@ public class Controller {
 	public static void main(String args[]) {
 
 		Scanner scan = new Scanner(System.in);
-		DataManagement model = new DataManagement();
+		ActionsHandler control = new ActionsHandler();
 
-		System.out.println("Welcome to the game. Your options are:\n1) New game\n2) Load game\n3) Exit");
-		String input = scan.nextLine().replaceAll("\n", "");
-
-		// Author: Jeremy Stiff
-		if (input.equals("1")) {
-			model.newGame();
-			for (System.out.println(model.getPlayerRoom().toString()), model.prompt(); scan.hasNextLine(); model.prompt()) {
-
-				input = scan.nextLine().replaceAll("\n", "").toLowerCase();
-
-				if (input.length() == 0)
-					continue;
-				else if (input.equals("n") || input.equals("north"))
-					model.movePlayerNorth();
-				else if (input.equals("s") || input.equals("south"))
-					model.movePlayerSouth();
-				else if (input.equals("e") || input.equals("east"))
-					model.movePlayerEast();
-				else if (input.equals("w") || input.equals("west"))
-					model.movePlayerWest();
-				else if (input.equals("x") || input.equals("exit")) {
-					System.out.println("Would you like to save? y/n");
-					input = scan.nextLine().replaceAll("\n", "").toLowerCase();
-					if (input.equals("y")) {
-						model.saveGame();
-						System.exit(0);
-					} else if (input.equals("n"))
-						System.exit(0);
-					else
-						continue;
-				}
-
-			}
-		} else if (input.equals("2")) {
-			model.loadSave();
-			for (System.out.println(model.getPlayerRoom().toString()), model.prompt(); scan.hasNextLine(); model.prompt()) {
-
-				input = scan.nextLine().replaceAll("\n", "").toLowerCase();
-
-				if (input.length() == 0)
-					continue;
-				else if (input.equals("n") || input.equals("north"))
-					model.movePlayerNorth();
-				else if (input.equals("s") || input.equals("south"))
-					model.movePlayerSouth();
-				else if (input.equals("e") || input.equals("east"))
-					model.movePlayerEast();
-				else if (input.equals("w") || input.equals("west"))
-					model.movePlayerWest();
-				else if (input.equals("x") || input.equals("exit")) {
-					System.out.println("Would you like to save? y/n");
-					input = scan.nextLine().replaceAll("\n", "").toLowerCase();
-					if (input.equals("y")) {
-						model.saveGame();
-						System.exit(0);
-					} else if (input.equals("n"))
-						System.exit(0);
-					else
-						continue;
-				}
-
-			}
-		} else if (input.equals("3"))
-			System.exit(0);
-		else
-			System.out.println("Incorrect input.");
-
+		control.welcomeMessage();
+		control.gameLoop();
+		
 	}
-
 
 }
